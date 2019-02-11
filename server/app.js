2cb3c91ece038468b4db74e61233ccf84eda679c
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-11 19:14:17 +0100

    Add body-parser dependency

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello from the MMS backend!'));
 
