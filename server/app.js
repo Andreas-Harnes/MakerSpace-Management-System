b45985a40b3aa64e924555a0d12509d2d1ab1737
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-11 20:01:13 +0100

    Add items route accepting GET requests

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
+const items = require('./routes/api/items');
 const port = 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api/users', users);
+app.use('/api/items', items);
 
 app.get('/', (req, res) => res.send('Hello from the MMS backend!'));
 
