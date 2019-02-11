c1b9dcda85860f17a49d9fa8a945227eb808df28
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-11 19:48:18 +0100

    Add users route accepting POST requests

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
 const port = 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/api/users', users);
 
 app.get('/', (req, res) => res.send('Hello from the MMS backend!'));
 
