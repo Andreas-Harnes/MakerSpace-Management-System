806b1fd281d8bb37db130fc895d23ed7d3039f43
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-13 19:29:08 +0100

    Add central database pool
    The pool allows for connections to MariaDB - connections can now be
    made to MariaDB.

---
+++ server/db/database.js
@@ -0,0 +1,11 @@
+const mariadb = require('mariadb');
+const config = require('../config');
+
+const pool = mariadb.createPool({
+    host: config.db.host,
+    user: config.db.user,
+    password: config.db.password,
+    database: config.db.name
+});
+
+exports.pool = pool;
