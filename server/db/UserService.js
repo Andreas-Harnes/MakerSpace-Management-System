67fa98eab97d459580b62b43bcb6eb315b1ed188
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-16 14:53:18 +0100

    Add query to fetchUser()

--- server/db/UserService.js
+++ server/db/UserService.js
@@ -5,6 +5,10 @@ class UserService {
     static async addUser(email) {
         return (await database.query('INSERT INTO user (email) VALUES (?)', [email])).insertId;
     }
+
+    static async fetchUser(id) {
+        return (await database.query('SELECT * FROM user WHERE id = ?', [id]))[0].id;
+    }
 }
 
 module.exports = UserService;
