1ae4e288eba46edbabfc1e8b12142dc5253d7797
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-17 18:28:16 +0100

    Send JSON web tokens to client after signup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,14 +1,13 @@
+const TokenUtils = require('../utils/token');
 const UserService = require('../services/user');
 
 module.exports = {
     signUp: async (req, res) => {
         if (await UserService.emailInUse(req.body.email)) {
-            return res.status(403).json({ error: 'Email is already in use.'});
+            return res.status(403).json({ error: 'Email already used.' });
         }
 
-        await UserService.addUser(req.body.email, req.body.password);
-
-        // TODO Generate ID and send to user.
-        res.status(201).send("New user created!");
+        const token = TokenUtils.sign(await UserService.addUser(req.body.email, req.body.password));
+        res.status(201).json({ token });
     }
 };
