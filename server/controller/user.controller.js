739cfa87b3f4c88f79ff9ab0e859d5299737f254
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-18 22:26:47 +0100

    User password is no longer stored plaintext

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcrypt');
 const UserService = require('../service/user.service');
 const TokenUtils = require('../utils/token');
+
+const saltLength = 12;
 
 module.exports = {
     signUp: async (req, res) => {
@@ -9,8 +12,16 @@ module.exports = {
             return res.status(403).json({ error: 'Email already used.' });
         }
 
-        const token = TokenUtils.sign(await UserService.create(req.body.email, req.body.password));
-        res.status(201).json({ token });
+        bcrypt.hash(req.body.password, saltLength, async (error, hash) => {
+            if (error) {
+                return res(500).json({ error });
+            }
+
+            const token = TokenUtils.sign(await UserService.create(req.body.email, hash));
+            res.status(201).json({ token });
+        });
+
+        
     },
 
     signIn: async (req, res) => {
