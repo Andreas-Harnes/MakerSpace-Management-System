10448b2da7860efce8c5985b3880bd7efd9cf5da
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-19 12:19:43 +0100

    Fix indentation

--- server/utils/route.js
+++ server/utils/route.js
@@ -3,14 +3,12 @@ const { check, validationResult } = require('express-validator/check');
 module.exports.validateAuth = [
     check('email').isEmail(),
 
-  // (?=.*\d)                 at least a digit
-  // (?=.*[a-z])              lowercase
-  // (?=.*[A-Z])              uppercase
-  // (?=.*[^a-zA-Z0-9])       ?
-  // .{8,}$/, "i"             at least 8 char(?)
-
-  check('password', 'Password is invalid').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
-
+    // (?=.*\d)                 at least a digit
+    // (?=.*[a-z])              lowercase
+    // (?=.*[A-Z])              uppercase
+    // (?=.*[^a-zA-Z0-9])       ?
+    // .{8,}$/, "i"             at least 8 char(?)
+    check('password', 'Password is invalid').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
