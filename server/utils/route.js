a75464b4fb966e3330dfd125fcc140f6da5dd183
Celina <22443141+celllaa95@users.noreply.github.com>
2019-02-20 16:22:45 +0100

    New password regex

--- server/utils/route.js
+++ server/utils/route.js
@@ -6,9 +6,10 @@ module.exports.validateAuth = [
     // (?=.*\d)                 at least a digit
     // (?=.*[a-z])              lowercase
     // (?=.*[A-Z])              uppercase
-    // (?=.*[^a-zA-Z0-9])       ?
-    // .{8,}$/, "i"             at least 8 char(?)
-    check('password').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+    // .{8,}$/, "i"             at least 8 char
+
+    check('password').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/, "i"),
+
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
