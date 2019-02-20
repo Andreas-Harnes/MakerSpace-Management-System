42e97396d5e9ddf4d12eec4aed821cadfd0a3293
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-02-20 16:39:52 +0100

    Update db model
    The former model was incomplete - the model has now been updated
    appropriately. The email is now unique and cannot be null. Additionally
    the password cannot be null.

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,11 +1,14 @@
 module.exports = (sequelize, Sequelize) => {
 	const User = sequelize.define('user', {
 	    email: {
-            type: Sequelize.STRING
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true
 	    },
         
         password: {
-		    type: Sequelize.STRING
+			type: Sequelize.STRING,
+			allowNull: false
 	    }
 	});
 	
