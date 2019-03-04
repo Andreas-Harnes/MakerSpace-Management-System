99ace97c0c67db1fd6257cbe1bb7d8b92f1541af
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-03-04 15:37:21 +0100

    Add Sequelize session store
    The session store ensures that the sessions will not be cleared should
    the app be restarted.

--- server/app.js
+++ server/app.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const Sequelize = require('sequelize');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const path = require('path');
 const db = require('./config/db.config.js');
 const passportConfig = require('./passport.config');
 const env = require('./config/env');
 const port = 3000;
 
-db.sequelize.sync({force: true}).then(() => {
-    console.log('DB synced ...');
-});
-
 const app = express();
 app.use(bodyParser.json());
+
+const sequelize = new Sequelize(
+    "database",
+    "username",
+    "password", {
+        "dialect": "sqlite",
+        "storage": "./session.sqlite"
+    }
+);
+
 app.use(session({
     name: 'mms.id',
     resave: false,
     saveUninitialized: false,
-    secret: env.cookie.secret
+    secret: env.cookie.secret,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 }));
 
 app.use(passportConfig.initialize());
 app.use(passportConfig.session());
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('DB synced ...');
+});
+
+sequelize.sync();
 
 app.use('/users', require('./route/users'));
 app.use('/cumulativeitems', require('./route/cumulativeitems'));
