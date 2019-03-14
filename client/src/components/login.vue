d0c7d1882cb21f79c215f02cc6b8d4955a315000
Andreas Harnes <andreas.harnes@hotmail.no>
2019-03-14 13:39:44 +0100

    Added functionality to sign out button

--- client/src/components/login.vue
+++ client/src/components/login.vue
@@ -78,7 +78,20 @@ export default {
       });
     },
     signoutRequest: function () {
-      console.log('signed out')
+      axios.get('/users/signout')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+      console.log('Complete')
+    });
+      
     }
   }
 }
