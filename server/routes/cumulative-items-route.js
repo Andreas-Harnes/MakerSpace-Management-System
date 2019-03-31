613ef2dc5a76b4295f9dc23c66007dcde1dd5789
Morten Kvigne <47319778+mkvigne@users.noreply.github.com>
2019-03-31 14:33:09 +0200

    Restrict number of items returned
    We don't want to send all the items at once as that will waste the
    bandwidth. The limit has been set to 25 for the time being but this can
    later be changed if desired.

--- server/routes/cumulative-items-route.js
+++ server/routes/cumulative-items-route.js
@@ -3,8 +3,10 @@ const db = require('../models');
 
 const router = express.Router();
 
+const MAX_ITEMS_PER_PAGE = 25;
+
 router.get('/', (req, res, next) => {
-    db.CumulativeItems.findAll()
+    db.CumulativeItems.findAll({ limit: MAX_ITEMS_PER_PAGE })
         .then(cumulativeItems => {
             return res.json({ cumulativeItems });
         })
