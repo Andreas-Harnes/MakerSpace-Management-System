21e1279065feb5ae45c7a7f723c888faf7ddea89
Celina <22443141+celllaa95@users.noreply.github.com>
2019-04-07 12:58:58 +0200

    ny body

--- client/src/components/body.vue
+++ client/src/components/body.vue
@@ -1,53 +1,25 @@
 
-  
-<template>
-<tr>
-    <b-container fluid>
- <!-- Søkeing -->
-      <template>
+
+<!-- body -->
+ <template>
+ <tr>
+<!-- search -->
+ <template>
     <div class="searching">
   <input type="text" v-model="search" placeholder="Search..." /> 
     </div> 
   </template> 
 
-  <!-- Checkbox-->
-  <template>
-<!--- content -->
-    </template>
-  <div id="container">
+<!-- containt -->
+ <div id="container">
     <div id="item_element">
       <h2 id="item_title">Element navn</h2>
       <img src="smiley.gif" alt="beskrivelse" height="42" width="42">
       <p id="description">Beskrivelse av element her</p>
     </div>
   </div>
-<template>
-<!--
-    <div class="checkbox-label">
-    <h5>Category</h5>
-    </div>
-
-    <div class="checkbox">
-      <label>Droner</label>
-        <input type="checkbox" value="droner" v-model="category"/>
-      <label>3D-printer</label>
-        <input type="checkbox" value="3d-printer" v-model="category"/>
-      </div>
-    <!--
-  <div class="category">
-    <b-form-group label="Category">
-      <b-form-checkbox-group
-        id="checkbox-group-1"
-        v-model="selected"
-        :options="options"
-        name="flavour-1"
-        stacked
-      ></b-form-checkbox-group>
-    </b-form-group> -->
-
-  
-
-  <div id="category">
+<!-- category -->
+<div id="category">
     <b-form-group label="Category">
       <b-form-checkbox-group id="checkbox-group-2" v-model="selected" name="flavour-2">
         <b-form-checkbox value="drone">Drone</b-form-checkbox>
@@ -58,12 +30,8 @@
       </b-form-checkbox-group>
     </b-form-group> 
   </div>
-  
-  <!-- <div>Selected: <strong>{{ selected }}</strong></div> -->
-  <!-- Availability -->
-  
-  
-   <div id="availability">
+  <!-- availability -->
+  <div id="availability">
     <b-form-group label="Availability">
       <b-form-checkbox-group id="checkbox-group-2" v-model="selected" name="flavour-3">
         <b-form-checkbox value="available">Available</b-form-checkbox>
@@ -72,113 +40,15 @@
       </b-form-checkbox-group>
     </b-form-group> 
   </div>
- 
- <!-- <div>Selected: <strong>{{ selected }}</strong></div> -->
- <!--
-   <div class="div-availability">
-    <div class="availability-label">
-    <h5>Availability</h5>
-    </div>
 
-<div class="availability">
-      <label>Available</label>
-        <input type="checkbox" value="available" v-model="availability"/>
-      <label>Unavailable</label>
-        <input type="checkbox" value="unavailable" v-model="availability"/>
-      </div>
-</div> 
--->
-</template>
-  
-<template>
-  <!--- --- Group by/sorting -->
-  <!--<div>
-    <h5 class="group-by">Group by</h5>
-    </div> -->
-</template>
-
-        <!--<b-row>
-            <b-col md="6" class="my-1">
-                <b-form-group label-cols-sm="3" class="mb-0" >
-                    <b-input-group>
-                        <b-form-select class="sorting" v-model="sortBy" :options="sortOptions">
-                            <option slot="first" :value="null">-- none --</option>
-                        </b-form-select>
-                    </b-input-group>
-                </b-form-group>
-            </b-col>
-        </b-row>
-        
-        <b-table id="Table"
-      selectable
-      responsive: true
-      show-empty
-      bordered="md"
-      :tabindex="0"
-      :items="items"
-      :aria-busy="isBusy"
-      :fields="fields"
-      :current-page="currentPage"
-      :per-page="perPage"
-      :sort-by.sync="sortBy"
-    > -->
-     
-   <!-- Busy state med spinner for loading-->
-    <div slot="table-busy" class="text-center text-danger my-2">
-      <b-spinner class="align-middle"/>
-      <strong>Loading items...</strong>
-      </div>
-
-      <!--<template class="test-2" slot="name" slot-scope="row">
-        {{ row.value.first }} {{ row.value.last }}
-      </template>
-      -->
-
-      <!--<template slot="actions" slot-scope="row">
-        <b-button size="sm" @click="trekkFra()" class="mr-1">FjernTing </b-button>
-        <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
-          leggTil
-        </b-button>
-        <b-button size="sm" @click="row.toggleDetails">
-          {{ row.detailsShowing ? 'Hide' : 'Show' }} Comment
-        </b-button>
-      </template> -->
-
-      <!--<template slot="row-details" slot-scope="row">
-        <b-card>
-          <ul>
-            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
-          </ul>
-        </b-card>
-      </template>
-    </b-table>
-    <!-- Side bytte igjennom pagination-->
-    <!--<b-row>
-      <b-col class="my-1">
-        <b-pagination aria-label="Table"
-          :total-rows="totalRows"
-          :per-page="perPage" label-page
-          v-model="currentPage"
-          first-text="First" label-first-page
-          prev-text="Prev" label-prev-page 
-          next-text="Next" label-next-page
-          last-text="Last" label-last-page
-          class="my-0"
-          align= "end"
-        />
-      </b-col>
-    </b-row>
-  </b-container>
   </tr>
-</template> -->
-
+ </template>
+ 
 <script>
 
 </script>
 
 <style lang="sass" scoped>
-
-
 
 </style>
 
