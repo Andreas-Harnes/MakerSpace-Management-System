d8894a3e142c317edfd36c09fcc06f7a3364e4fd
Andreas Harnes <andreas.harnes@hotmail.no>
2019-04-07 15:30:53 +0200

    Added paths

--- client/src/main.js
+++ client/src/main.js
@@ -7,12 +7,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import Nav from './components/nav'
-import Login from './components/login'
-import Register from './components/register'
-import Header from './components/header'
-import body from './components/body'
+import login from './components/login'
+import register from './components/register'
 import header from './components/header'
+import body from './components/body'
+// import header from './components/header'
 import test from './components/test'
+import forgotten_password from './components/forgotten_password'
+import faq from './components/faq'
+import tos from './components/tos'
+import privacy_policy from './components/privacy_policy'
+import settings from './components/settings'
+import settings from './components/item'
 
 //tell vue to use bootstrap
 Vue.use(BootstrapVue)
@@ -23,11 +29,16 @@ Vue.use(VueRouter)
 //d efine your routes
 const routes = [
   { path: '/', component: body },
-  { path: '/header', component: Header },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  // { path: '/body' , component: body},
-  { path: '/test' , component: test}
+  { path: '/header', component: header },
+  { path: '/login', component: login },
+  { path: '/register', component: register },
+  { path: '/test' , component: test},
+  { path: '/forgotten' , component: forgotten_password},
+  { path: '/faq' , component: faq},
+  { path: '/privacy' , component: privacy_policy},
+  { path: '/tos' , component: tos},
+  { path: '/settings' , component: settings},
+  { path: '/item' , component: item},
 ]
 
 // Create the router instance and pass the `routes` option
