4fbfe1dd5fa4617a451ccb41d521ed4f6201f448
Morten Kvigne <49249102+mkvigne@users.noreply.github.com>
2019-04-10 09:03:01 +0200

    Finish JSON response when fetching items

--- server/routes/cumulative-items-route.js
+++ server/routes/cumulative-items-route.js
@@ -20,7 +20,40 @@ router.get('/', (req, res, next) => {
         offset: startPosition
     })
         .then(cumulativeItems => {
-            return res.json({ cumulativeItems });
+            let entries = [];
+            for (let i = 0; i < cumulativeItems.length; i++) {
+                entries[i] = cumulativeItems[i].id;
+            }
+
+            db.SpecificItems.findAll({
+                where: {
+                    cumulativeItemsId: entries
+                }
+            })
+                .then(specificItems => {
+                    for (let i = 0; i < cumulativeItems.length; i++) {
+                        cumulativeItems[i].dataValues.items = 0;
+                        cumulativeItems[i].dataValues.availableItems = 0;
+                    }
+                    
+                    for (let i = 0; i < specificItems.length; i++) {
+                        for (let j = 0; j < cumulativeItems.length; j++) {
+                            if (cumulativeItems[j].dataValues.id === specificItems[i].dataValues.cumulativeItemsId) {
+                                cumulativeItems[j].dataValues.items++;
+                                if (specificItems[i].dataValues.status) {
+                                    cumulativeItems[j].dataValues.availableItems++;
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+
+                    return res.json({ cumulativeItems });
+                })
+                .catch(error => {
+                    return next(error);
+                });
         })
         .catch(error => {
             return next(error);
